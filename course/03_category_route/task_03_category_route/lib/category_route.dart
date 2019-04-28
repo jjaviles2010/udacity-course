1e3b884bc925dcfd8115b6da6d5be0696c8f3bc1
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2019-04-28 16:27:14 -0300

    Creating Menu with List of Categories

--- course/03_category_route/task_03_category_route/lib/category_route.dart
+++ course/03_category_route/task_03_category_route/lib/category_route.dart
@@ -3,10 +3,12 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:task_03_category_route/category.dart';
 
-// TODO: Check if we need to import anything
+// COMPLETED: Check if we need to import anything
 
-// TODO: Define any constants
+// COMPLETED: Define any constants
+final _backgroundColor = Colors.green[100];
 
 /// Category Route (screen).
 ///
@@ -16,8 +18,6 @@ import 'package:flutter/material.dart';
 /// While it is named CategoryRoute, a more apt name would be CategoryScreen,
 /// because it is responsible for the UI at the route's destination.
 class CategoryRoute extends StatelessWidget {
-  const CategoryRoute();
-
   static const _categoryNames = <String>[
     'Length',
     'Area',
@@ -40,21 +40,51 @@ class CategoryRoute extends StatelessWidget {
     Colors.red,
   ];
 
+  const CategoryRoute();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: Create a list of the eight Categories, using the names and colors
+    // COMPLETED: Create a list of the eight Categories, using the names and colors
     // from above. Use a placeholder icon, such as `Icons.cake` for each
     // Category. We'll add custom icons later.
+    final categories = <Category>[];
+
+    for (var i = 0; i < _categoryNames.length; i++) {
+      categories.add(Category(
+        name: _categoryNames[i],
+        color: _baseColors[i],
+        iconLocation: Icons.cake,
+      ));
+    }
 
     // TODO: Create a list view of the Categories
-    final listView = Container();
+    final listView = Container(
+      color: _backgroundColor,
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: _buildCategoryWidgets(categories),
+    );
 
-    // TODO: Create an App Bar
-    final appBar = AppBar();
+    // COMPLETED: Create an App Bar
+    final appBar = AppBar(
+      title: Text(
+        'Unit Converter',
+        style: TextStyle(fontSize: 30.0, color: Colors.black),
+      ),
+      elevation: 0.0,
+      centerTitle: true,
+      backgroundColor: _backgroundColor,
+    );
 
     return Scaffold(
       appBar: appBar,
       body: listView,
+    );
+  }
+
+  Widget _buildCategoryWidgets(List<Widget> categories) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) => categories[index],
+      itemCount: categories.length,
     );
   }
 }
